5bf261953b2f408963234bd6db7a42eece4c6794
MUHAMMAD YUSUF ABDURROHMAN <mylearning1505@gmail.com>
2026-02-14 19:47:16 +0000

    Apply gradle fixes from user again

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("com.chaquo.python") version "15.0.1"
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
