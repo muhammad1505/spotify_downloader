4dae0eddfae0ae22168d9772063bd0a64c661637
MUHAMMAD YUSUF ABDURROHMAN <mylearning1505@gmail.com>
2026-02-14 19:59:12 +0000

    Remove redundant plugin declarations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.chaquo.python") version "15.0.1"
+    id("com.chaquo.python")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("com.android.library") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-}
 
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
